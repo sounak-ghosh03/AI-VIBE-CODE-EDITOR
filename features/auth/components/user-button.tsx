99f2302d681916d71ba45593b6f283292af389c8
SOUNAK GHOSH <sounak2309@gmail.com>
2026-01-04 19:27:27 +0530

    added user button

--- features/auth/components/user-button.tsx
+++ features/auth/components/user-button.tsx
@@ -1,14 +1,49 @@
 "use client";
-import { User } from "lucide-react";
-import { useSession } from "next-auth/react";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-export const useCurrentUser = () => {
-  const session = useSession();
-  return session?.data?.user;
-};
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { LogOut, User } from "lucide-react";
+import LogoutButton from "./logout-button";
+import { useCurrentUser } from "../hooks/use-current-user";
 
 const UserButton = () => {
   const user = useCurrentUser();
-  return user ? <User /> : null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className={cn("relative rounded-full")}>
+          <Avatar>
+            <AvatarImage src={user?.image!} alt={user?.name!} />
+            <AvatarFallback className="bg-red-500">
+              <User className="text-white" />
+            </AvatarFallback>
+          </Avatar>
+        </div>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="mr-4">
+        <DropdownMenuItem>
+          <span>{user?.email}</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <LogoutButton>
+          <DropdownMenuItem>
+            <LogOut className="h-4 w-4 mr-2" />
+            LogOut
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
+
 export default UserButton;
